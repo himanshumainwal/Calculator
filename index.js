5eb1233e0b981ce68034b701e67f4c1c5649deda
Himanshu <himanshu@Himanshus-MacBook-Pro.local>
2023-11-06 14:02:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let string = "";
+let buttons = document.querySelectorAll(".button");
+let input = document.querySelector("input");
+
+let arr = Array.from(buttons).forEach((button) => {
+    button.addEventListener('click' , (e)=>{
+
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }else if (e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }else if (e.target.innerHTML == 'DE'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }else {
+            string = string + e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
